ed9d9ce4489af68a36c38e0647664cc32250f677
mohamed <mohamed@gmail.com>
2024-10-25 12:23:18 +0100

    vesion 3

--- task1.c
+++ task1.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int choix,i,number;
-char del;
+#define LEFT(str, w) \
+    ({int m = w + strlen(str); m % 2 ? (m + 1) / 2 : m / 2;})
+
+#define RIGHT(str, w) \
+({ int m = w - strlen(str); m % 2 ? (m - 1) / 2 : m / 2; })
+
+#define STR_CENTER(str, width) \
+    LEFT(str, width), str, RIGHT(str, width), ""
+
+
+
+
+
+
+
+int choix,i,number,found,Size=0;
+char del[30];
+
+
+// address structure
 
 struct adresse{
 
@@ -9,27 +29,33 @@ struct adresse{
     char ville[30];
     int code_postal;
 
-
-
 };
 
-typedef struct {
+// personne structure
 
+typedef struct {
+    int id;
     char name[30];
     int age;
    struct adresse add;
 
 }Personne;
 
-
+    int Add();
+    int Delete(Personne p[]);
+    int Update(Personne p[]);
+    int Read(Personne p[]);
 
 
 int main (){
 
+// caling add function first to add data
+// inside data after finishing adding data we are going to show up a menu to update delete ...
 
-  Add();
 
 
+
+    Add();
 
     return 0;
 
@@ -37,18 +63,18 @@ int main (){
 
 int entery(Personne p[] ){
 
+    // Menu the select what users want to do more  it comes after add() function
      do{
     printf("\n\n #################Menu################\n");
 
       printf("    1. Update  data # \n\n");
         printf("    2. Read  data # \n\n");
           printf("    3. Delete  data # \n\n");
+           printf("    4. Add new data  data # \n\n");
 
            printf(" -->Enter the number related to your choose : ");
           scanf("%d",&choix);
-
-
-
+// check choix and move to funtions
 
           switch(choix){
           case 1:
@@ -65,12 +91,14 @@ int entery(Personne p[] ){
             case 3:
               Delete(p);
 
-
             break;
 
+            case 4:
+                Add(p);
+                number=number+1;
+                break;
+            default: printf("You should Enter a number Related to The Tabel");
 
-
-            break;
 
           }
 
@@ -82,16 +110,20 @@ int entery(Personne p[] ){
 
 int Add(){
 
-      printf("\n##############Add Menu################# \n\n",i+1);
+     printf("%*s%*s\n\n", STR_CENTER("<<<<<<<<<< Add >>>>>>>>>>", 40));
+
+// printf("\n <<<<<<<<<< Add >>>>>>>>>>\n\n",i+1);
     printf(" \n  >> number  of person wanted to add :  ");
+
    scanf("%d",&number);
+
 
    Personne p[number];
 
 
-
         for(i=0;i<number;i++){
-                printf("\n**************Person %d****************\n",i+1);
+
+            printf("\n**************Person %d****************\n",i+1);
                 printf(" -> Enter the name :  ");
           scanf(" %[^\n]s",&p[i].name);
          printf(" -> Enter the age :  ");
@@ -105,6 +137,10 @@ int Add(){
           scanf("%s",&p[i].add.ville);
            printf(" -> Enter the code postal  :  ");
           scanf("%d",&p[i].add.code_postal);
+          p[i].id=i+1;
+
+          Size=Size+i+1;
+
 
         }
 
@@ -115,49 +151,171 @@ int Add(){
 
 }
 int Delete(Personne pers[]){
+    found=-1;
 
-    printf("\n###########Delete#############\n");
+    printf("\n  <<<<<<<<<<<Delete>>>>>>>>>>\n");
 
-      printf("\n Enter Name of person wanted to Delete\n");
-      scanf("%[^\n]d",&del);
+    if(Size==0){
 
-     /* for(i=0;i<number;i++){
-            if(del==pers[i].name){
+            printf("      No Data to Display Add new    \n ");
 
-                    printf("A user match found");
+    Add();
+}
 
+else{
+
+
+
+
+      printf("\n Enter Name of person wanted to Delete : ");
+      scanf(" %[^\n]s",&del);
+
+
+
+      for(i=0;i<number;i++){
+
+            if(sizeof(pers)>=0&&strcmp(del,pers[i].name)==0)
+            {
+                found=i;
+                break;
             }
 
 
+      }
+
+      if(found>=0){
+
+
+
+        for(i=found;i<number-1;i++)
+        {
+            pers[i]=pers[i+1];
+        }
+
+        number--;
+        Size--;
+        printf("<<<<<<<<<person %d Deleted >>>>>>>>>>",pers[i].id);
+
+        entery(pers);
+      }
+
+      else {
+            printf("<<<<<<<<<No Person Find>>>>>>>>>>>>>>");
+      entery(pers);
+      }
+
 
 
 
 
 
 }
- */
-
-
 
 
 }
+
+
+
+
+
 int Read(Personne pers[]){
 
-printf("\n###########Read#############\n");
+printf("\n  <<<<<<<<<< Read >>>>>>>>>>\n");
 
+if(Size==0){
 
+            printf("      No Data to Display Add new    \n ");
 
+    Add();
+}
+
+else {
 for(i=0;i<number;i++){
 
 
 
-      printf("\n\n ***The Person %d  Data ***\n \n    >>name is : %s \n    >> age is : %d \n    >> the street : %s \n    >> the city :%s \n    >> code postal : %d \n\n\n",i+1,pers[i].name,pers[i].age,pers[i].add.rue,pers[i].add.ville,pers[i].add.code_postal);
+      printf("\n\n ***The Person %d  Data ***\n \n    >>name is : %s \n    >> age is : %d \n    >> the street : %s \n    >> the city :%s \n    >> code postal : %d \n\n\n",pers[i].id,pers[i].name,pers[i].age,pers[i].add.rue,pers[i].add.ville,pers[i].add.code_postal);
 
 
 }
-
-
-
-
 }
-int Update(Personne pers[]){}
+entery(pers);
+}
+
+
+int Update(Personne pers[]){
+
+
+    if(Size==0){
+
+            printf("      No Data to Display Add new    \n ");
+
+       Add();
+
+    }
+
+   else{
+
+ found=-1;
+
+    printf("Enter Id of table you want to Update : ");
+    scanf("%d",&choix);
+
+
+    for(i=0;i<number;i++){
+            if(pers[choix-1].id==choix)
+            {
+                found=i+1;
+            }
+
+
+
+    }
+
+
+
+
+    if(found>=0){
+
+            found--;
+
+             printf("\n**************Update of Person %d****************\n",found+1);
+                printf(" -> Enter the name :  ");
+          scanf(" %[^\n]s",&pers[found].name);
+
+         printf(" -> Enter the age :  ");
+          scanf("%d",&pers[found].age);
+
+
+        printf(" -> Enter the street  :  ");
+          scanf("%s",&pers[found].add.rue);
+
+           printf(" -> Enter the city  :  ");
+          scanf("%s",&pers[found].add.ville);
+           printf(" -> Enter the code postal  :  ");
+          scanf("%d",&pers[found].add.code_postal);
+          pers[found].id=i+1;
+
+          entery(pers);
+
+        }
+
+        else {
+                printf("The Id Entered has no Data \n\n\n");
+
+                Add();
+
+                }
+
+   }
+
+
+    }
+
+
+
+
+
+
+
+
